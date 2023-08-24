0ca46d0ab7dbf7b369cc8a3da28f50876c73b17c
joaosantosmedeiros <jopesame@gmail.com>
2023-08-24 16:29:00 -0300

    add modulos funcionarios, buscar funcionarios e cadastrar funcinarios

--- main.c
+++ main.c
@@ -1,27 +1,21 @@
-///////////////////////////////////////////////////////////////////////////////
-///             Universidade Federal do Rio Grande do Norte                 ///
-///                 Centro de Ensino Superior do Seridó                     ///
-///               Departamento de Computação e Tecnologia                   ///
-///                  Disciplina DCT1106 -- Programação                      ///
-///                  Projeto Sistema de Gestão Escolar                      ///
-///             Developed by @joaosantosmedeiros -- since Ago, 2023         ///
-///////////////////////////////////////////////////////////////////////////////
-///                                Semana 0                                 ///
-///////////////////////////////////////////////////////////////////////////////
-
 #include <stdio.h>
 #include <stdlib.h>
 
 void tela_menu_principal(void);
-void tela_sobre();
-void tela_equipe();
-
+void tela_sobre(void);
+void tela_equipe(void);
+void modulo_funcionarios(void);
+void buscar_funcionario(void);
+void cadastrar_funcionario(void);
 
 // Programa principal
 int main(void) {
     tela_menu_principal();
     tela_sobre();
     tela_equipe();
+    modulo_funcionarios();
+    buscar_funcionario();
+    cadastrar_funcionario();
     return 0;
 }
 
@@ -48,6 +42,7 @@ void tela_menu_principal(void) {
     printf("|||            2. Módulo Departamento                                       |||\n");
     printf("|||            3. Módulo Desempenho                                         |||\n");
     printf("|||            4. Módulo Relatórios                                         |||\n");
+    printf("|||            5. Login                                                     |||\n");
     printf("|||            0. Sair                                                      |||\n");
     printf("|||                                                                         |||\n");
     printf("|||            Escolha a opção desejada: ");
@@ -116,3 +111,109 @@ void tela_equipe(void) {
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
 } 
+
+
+void modulo_funcionarios(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||            ===================================================          |||\n");
+    printf("|||            = = = = = = = = = = = = = = = = = = = = = = = = = =          |||\n");
+    printf("|||            = = = =  Sistema de Gestão de Funcionários  = = = =          |||\n");
+    printf("|||            = = = = = = = = = = = = = = = = = = = = = = = = = =          |||\n");
+    printf("|||            ===================================================          |||\n");
+    printf("|||            Developed by @joaosantosmedeiros -- since Ago, 2023          |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                           Módulo Funcionários                           |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||            1. Buscar Funcionário                                        |||\n");
+    printf("|||            2. Cadastrar Funcionário                                     |||\n");
+    printf("|||            3. Atualizar Funcionário                                     |||\n");
+    printf("|||            4. Deletar Funcionário                                       |||\n");
+    printf("|||            0. Sair                                                      |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||            Escolha a opção desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("|||                                                                         |||\n");
+    printf("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+void buscar_funcionario(void) {
+    char cpf[11];
+    system("clear||cls");
+    printf("\n");
+    printf("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||            ===================================================          |||\n");
+    printf("|||            = = = = = = = = = = = = = = = = = = = = = = = = = =          |||\n");
+    printf("|||            = = = =  Sistema de Gestão de Funcionários  = = = =          |||\n");
+    printf("|||            = = = = = = = = = = = = = = = = = = = = = = = = = =          |||\n");
+    printf("|||            ===================================================          |||\n");
+    printf("|||            Developed by @joaosantosmedeiros -- since Ago, 2023          |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                         Busca de Funcionários                           |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||_________________________________________________________________________|||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                         Digite o CPF do funcionário: ");
+    scanf("%s", cpf);
+    getchar();
+    printf("|||                                                                         |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void cadastrar_funcionario(void) {
+    char cpf[11], nome[30], email[30], senha[30], confirm_senha[30], telefone[30], endereco[50];
+    system("clear||cls");
+    printf("\n");
+    printf("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||            ===================================================          |||\n");
+    printf("|||            = = = = = = = = = = = = = = = = = = = = = = = = = =          |||\n");
+    printf("|||            = = = =  Sistema de Gestão de Funcionários  = = = =          |||\n");
+    printf("|||            = = = = = = = = = = = = = = = = = = = = = = = = = =          |||\n");
+    printf("|||            ===================================================          |||\n");
+    printf("|||            Developed by @joaosantosmedeiros -- since Ago, 2023          |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                        Cadastro de Funcionários                         |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||_________________________________________________________________________|||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                    Digite o CPF do funcionário: ");
+    scanf("%s", cpf);
+    printf("|||                    Digite o nome do funcionário: ");
+    scanf("%s", nome);
+    printf("|||                    Digite o email do funcionário: ");
+    scanf("%s", email);
+    printf("|||                    Digite a senha do funcionário: ");
+    scanf("%s", senha);
+    printf("|||                    Confirme a senha do funcionário: ");
+    scanf("%s", confirm_senha);
+    printf("|||                    Digite o telefone do funcionário: ");
+    scanf("%s", telefone);
+    printf("|||                    Digite o endereco do funcionário: ");
+    scanf("%s", endereco);
+    getchar();
+    printf("|||                                                                         |||\n");
+    printf("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
