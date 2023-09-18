4b3ac9bdc34013dfbda40b2502924195731ecc1b
joaosantosmedeiros <jopesame@gmail.com>
2023-09-18 15:28:47 -0300

    modularização das funções de desempenhos

---
+++ desempenhos.h
@@ -0,0 +1,10 @@
+#ifndef DESEMPENHOS_H
+#define DESEMPENHOS_H
+
+void modulo_desempenhos(void);
+void buscar_desempenho(void);
+void cadastrar_desempenho(void);
+void atualizar_desempenho(void);
+void deletar_desempenho(void);
+
+#endif
